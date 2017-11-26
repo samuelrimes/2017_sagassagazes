9f0d4b5ed85b3ed353f28c401362d75febcb38c4
Samuel <samucarimesg@hotmail.com>
2017-11-26 21:46:52 -0200

    trabalho design web 4º bimestre.

---
+++ js/dados.js
@@ -0,0 +1,131 @@
+var filmes = [{"id":1
+,"titulo":"Harry Potter e a Pedra Filosofal"
+,"img1":"./css/pedrafilosofal2.jpg"
+,"img2":"./css/pedrafilosofal3.jpg"
+,"texto":"Conheça Harry Potter, um menino que soube em seu aniversário de onze anos que é filho órfão de dois bruxos e possui poderes mágicos únicos. De filho indesejado, passa a ser um estudante de Hogwarts, uma escola inglesa para bruxos. Lá ele conhece vários amigos que o ajudam a descobrir a verdade sobre as mortes misteriosas de seus pais"
+,"data":"23 de novembro de 2001"
+,"diretor":"Chris Columbus"
+,"premios":"Satellite Awards - Melhor Revelação: Rupert Grint/ Saturn Award para Melhor Figurino/ Costume Designers Guild Award - Prêmio de Excelência em Figurino para filmes de fantasia/ Evening Standard British Film Award for best technical/artistic achievement/ Young Artist Awards Melhor atriz jovem coadjuvante/secundária: Emma Watson/ Critics' Choice Award: Melhor Filme de Família."}
+,{"id":2
+,"titulo":"Harry Potter e a Camara Secreta"
+,"img1":"./css/camarasecreta2.jpg"
+,"img2":"./css/camarasecreta3.jpg"
+,"texto":"Após as sofríveis férias na casa dos tios, Harry Potter se prepara para voltar a Hogwarts e começar seu segundo ano na escola de bruxos. Na véspera do início das aulas, a estranha criatura Dobby aparece em seu quarto e o avisa de que voltar é um erro e que algo muito ruim pode acontecer se Harry insistir em continuar os estudos de bruxaria. O garoto, no entanto, está disposto a correr o risco e se livrar do lar problemático"
+,"data":"22 de novembro de 2002"
+,"diretor":"Chris Columbus"
+,"premios":"Otto Award: Melhor Atriz/ Critics' Choice Award: Melhor Filme de Família/ Critics' Choice Award: Melhor Compositor"}
+,{"id":3
+,"titulo":"Harry Potter e o Prisioneiro de Azkaban"
+,"img1":"./css/prisioneiro2.jpg"
+,"img2":"./css/prisioneiro3.jpg"
+,"texto":"O terceiro ano de Harry Potter em Hogwarts começa mal quando ele descobre que o assassino Sirius Black escapou da prisão de Azkaban e está empenhado em matá-lo. Enquanto o gato de Hermione atormenta o rato doente de Ron, um bando de dementadores são enviados para proteger a escola de Sirius Black. Um professor misterioso ajuda Harry a aprender a se defender"
+,"data":"4 de junho de 2004"
+,"diretor":"Alfonso Cuarón"
+,"premios":"Indicações ao Oscar: Melhores Efeitos Especiais e Melhor Trilha Sonora/ Indicações ao BAFTA: Melhor Filme Britânico, Melhor Maquiagem, Melhores Efeitos Especiais e Melhor Desenho de Produção."}
+,{"id":4
+,"titulo":"Harry Potter e o Cálice de Fogo"
+,"img1":"./css/calicedefogo2.jpg"
+,"img2":"./css/calicedefogo3.jpg"
+,"texto":"Harry retorna para seu quarto ano na Escola de Magia e Bruxaria de Hogwarts, junto com os seus amigos Rony e Hermione. Desta vez, acontece um torneio entre as três maiores escola de magia, com um participante selecionado de cada escola pelo Cálice de Fogo. O nome de Harry aparece, mesmo não tendo se inscrito, e ele precisa competir."
+,"data":"25 de novembro de 2005"
+,"diretor":"Mike Newell"
+,"premios":" BAFTA de Melhor Desenho de Produção/ Indicado para o Oscar de Melhor Direção de Arte."}
+,{"id":5
+,"titulo":"Harry Potter e a Ordem da Fênix"
+,"img1":"./css/ordemdafenix2.jpg"
+,"img2":"./css/ordemdafenix3.jpg"
+,"texto":"Harry está em seu quinto ano em Hogwarts. Ele acaba ouvindo que muitos não sabem a verdade sobre seu encontro com Lord Voldermort. O Ministro de Mágica, Cornelius Fudge, indica Dolores Umbridge para ser a nova professora de Defesa contra as Artes Negras, por acreditar que Dumbledore planeja tomar seu lugar. Porém, os métodos que ela usa são totalmente inapropriados; Harry, então, se reúne com um grupo de alunos para defender sua escola."
+,"data":"11 de julho de 2007"
+,"diretor":"David Yates"
+,"premios":"Prêmio do Cinema Europeu: Prêmio do Público/ National Movie Award: Melhor atriz: Emma Watson/ Teen Choice Award: Melhor Filme de Ação/Aventura do Verão/ People's Choice Award: Melhor Filme de Drama."}
+,{"id":6
+,"titulo":"Harry Potter e o Enigma do Príncipe"
+,"img1":"./css/enigmadoprincipe2.jpg"
+,"img2":"./css/enigmadoprincipe3.jpg"
+,"texto":"Os Comensais da Morte estão empolgados com a volta do Voldemort e atacam o mundo dos trouxas e dos bruxos. Dumbledore chama seu amigo Horácio Slughorn para ajudá-lo na preparação de Harry Potter. Harry se vê cada vez mais interessado em Gina e Hermonie morre de ciúmes de Ron, mas prefere não dizer nada"
+,"data":"15 de julho de 2009"
+,"diretor":"David Yates"
+,"premios":"Scream Awards: Melhor cena do Ano e Melhor Figurino/ Teen Choice Awards: Melhor filme de aventura do verão/ Golden Flip: Filme do Ano/ Oscar: Indicado em Melhor Fotografia/ MTV Movie Awards: Melhor Vilão - Tom Felton como Draco Malfoy/ National Movie Awards: Venceu na categoria de Melhor Filme Familiar."}
+,{"id":7
+,"titulo":"Harry Potter e as Relíquias da Morte: P 1"
+,"img1":"./css/reliquiasdamorte2.jpg"
+,"img2":"./css/reliquiasdamorte3.jpg"
+,"texto":"Sem a orientação e a proteção de seus professores, Harry, Rony e Hermione começam uma missão para destruir as Horcruxes, que são fontes da imortalidade de Voldemort. Mais do que nunca, eles dependem uns dos outros, mas forças obscuras ameaçam separá-los."
+,"data":"19 de novembro de 2010"
+,"diretor":"David Yates"
+,"premios":"Oscar: Indicado em Melhor Direção de Arte e Melhores Efeitos Especiais/ VES Awards: Melhor Personagem Animado em Filme - Dobby/ Jameson Empire Awards: Melhor Filme de Ficção Científica/ Fantasia/ National Movie Awards: Melhor Filme de Fantasia/ MTV Movie Awards: Melhor Vilão - Tom Felton/ Teen Choice Awards 2011: Melhor filme de Ficção Científica/Fantasia e em Melhor atriz de filme de Ficção Científica/Fantasia - Emma Watson"}
+,{"id":8
+,"titulo":"Harry Potter e as Relíquias da Morte: P 2"
+,"img1":"./css/reliquiasdamorte4.jpg"
+,"img2":"./css/reliquiasdamorte5.jpg"
+,"texto":"No desfecho épico, a batalha entre as forças do bem e do mal da magia alcançam o mundo dos trouxas. O risco nunca foi tão grande e ninguém está seguro. Harry Potter fará um sacrifício final conforme o confronto com Lord Voldermort se aproxima."
+,"data":"15 de julho de 2011"
+,"diretor":"David Yates"
+,"premios":"People's Choice Award: Filme Favorito/ MTV Movie Awards: Melhor elenco - Daniel Radcliffe, Rupert Grint, Emma Watson e Tom Felton/ MTV Movie Award: Melhor Herói - Daniel Radcliffe/ Prêmio BAFTA de Cinema: Melhores Efeitos Visuais/ People's Choice Award: Filme de Ação Favorito/ People's Choice Award: Adaptação de Livro Favorita/ Critics' Choice Award: Melhor Maquiagem/ Empire Award: Melhor Filme/ muitos outros."
+}];
+
+var livros = [{"id":1
+,"titulo":"Harry Potter e a Pedra Filosofal"
+,"img1":"./css/pedrafilosofal2.jpg"
+,"img2":"./css/pedrafilosofal3.jpg"
+,"texto":"Conheça Harry Potter, um menino que soube em seu aniversário de onze anos que é filho órfão de dois bruxos e possui poderes mágicos únicos. De filho indesejado, passa a ser um estudante de Hogwarts, uma escola inglesa para bruxos. Lá ele conhece vários amigos que o ajudam a descobrir a verdade sobre as mortes misteriosas de seus pais"
+,"data":"23 de novembro de 2001"
+,"diretor":"Chris Columbus"
+,"premios":"Satellite Awards - Melhor Revelação: Rupert Grint/ Saturn Award para Melhor Figurino/ Costume Designers Guild Award - Prêmio de Excelência em Figurino para filmes de fantasia/ Evening Standard British Film Award for best technical/artistic achievement/ Young Artist Awards Melhor atriz jovem coadjuvante/secundária: Emma Watson/ Critics' Choice Award: Melhor Filme de Família."}
+,{"id":2
+,"titulo":"Harry Potter e a Camara Secreta"
+,"img1":"./css/camarasecreta2.jpg"
+,"img2":"./css/camarasecreta3.jpg"
+,"texto":"Após as sofríveis férias na casa dos tios, Harry Potter se prepara para voltar a Hogwarts e começar seu segundo ano na escola de bruxos. Na véspera do início das aulas, a estranha criatura Dobby aparece em seu quarto e o avisa de que voltar é um erro e que algo muito ruim pode acontecer se Harry insistir em continuar os estudos de bruxaria. O garoto, no entanto, está disposto a correr o risco e se livrar do lar problemático"
+,"data":"22 de novembro de 2002"
+,"diretor":"Chris Columbus"
+,"premios":"Otto Award: Melhor Atriz/ Critics' Choice Award: Melhor Filme de Família/ Critics' Choice Award: Melhor Compositor"}
+,{"id":3
+,"titulo":"Harry Potter e o Prisioneiro de Azkaban"
+,"img1":"./css/prisioneiro2.jpg"
+,"img2":"./css/prisioneiro3.jpg"
+,"texto":"O terceiro ano de Harry Potter em Hogwarts começa mal quando ele descobre que o assassino Sirius Black escapou da prisão de Azkaban e está empenhado em matá-lo. Enquanto o gato de Hermione atormenta o rato doente de Ron, um bando de dementadores são enviados para proteger a escola de Sirius Black. Um professor misterioso ajuda Harry a aprender a se defender"
+,"data":"4 de junho de 2004"
+,"diretor":"Alfonso Cuarón"
+,"premios":"Indicações ao Oscar: Melhores Efeitos Especiais e Melhor Trilha Sonora/ Indicações ao BAFTA: Melhor Filme Britânico, Melhor Maquiagem, Melhores Efeitos Especiais e Melhor Desenho de Produção."}
+,{"id":4
+,"titulo":"Harry Potter e o Cálice de Fogo"
+,"img1":"./css/calicedefogo2.jpg"
+,"img2":"./css/calicedefogo3.jpg"
+,"texto":"Harry retorna para seu quarto ano na Escola de Magia e Bruxaria de Hogwarts, junto com os seus amigos Rony e Hermione. Desta vez, acontece um torneio entre as três maiores escola de magia, com um participante selecionado de cada escola pelo Cálice de Fogo. O nome de Harry aparece, mesmo não tendo se inscrito, e ele precisa competir."
+,"data":"25 de novembro de 2005"
+,"diretor":"Mike Newell"
+,"premios":" BAFTA de Melhor Desenho de Produção/ Indicado para o Oscar de Melhor Direção de Arte."}
+,{"id":5
+,"titulo":"Harry Potter e a Ordem da Fênix"
+,"img1":"./css/ordemdafenix2.jpg"
+,"img2":"./css/ordemdafenix3.jpg"
+,"texto":"Harry está em seu quinto ano em Hogwarts. Ele acaba ouvindo que muitos não sabem a verdade sobre seu encontro com Lord Voldermort. O Ministro de Mágica, Cornelius Fudge, indica Dolores Umbridge para ser a nova professora de Defesa contra as Artes Negras, por acreditar que Dumbledore planeja tomar seu lugar. Porém, os métodos que ela usa são totalmente inapropriados; Harry, então, se reúne com um grupo de alunos para defender sua escola."
+,"data":"11 de julho de 2007"
+,"diretor":"David Yates"
+,"premios":"Prêmio do Cinema Europeu: Prêmio do Público/ National Movie Award: Melhor atriz: Emma Watson/ Teen Choice Award: Melhor Filme de Ação/Aventura do Verão/ People's Choice Award: Melhor Filme de Drama."}
+,{"id":6
+,"titulo":"Harry Potter e o Enigma do Príncipe"
+,"img1":"./css/enigmadoprincipe2.jpg"
+,"img2":"./css/enigmadoprincipe3.jpg"
+,"texto":"Os Comensais da Morte estão empolgados com a volta do Voldemort e atacam o mundo dos trouxas e dos bruxos. Dumbledore chama seu amigo Horácio Slughorn para ajudá-lo na preparação de Harry Potter. Harry se vê cada vez mais interessado em Gina e Hermonie morre de ciúmes de Ron, mas prefere não dizer nada"
+,"data":"15 de julho de 2009"
+,"diretor":"David Yates"
+,"premios":"Scream Awards: Melhor cena do Ano e Melhor Figurino/ Teen Choice Awards: Melhor filme de aventura do verão/ Golden Flip: Filme do Ano/ Oscar: Indicado em Melhor Fotografia/ MTV Movie Awards: Melhor Vilão - Tom Felton como Draco Malfoy/ National Movie Awards: Venceu na categoria de Melhor Filme Familiar."}
+,{"id":7
+,"titulo":"Harry Potter e as Relíquias da Morte: P 1"
+,"img1":"./css/reliquiasdamorte2.jpg"
+,"img2":"./css/reliquiasdamorte3.jpg"
+,"texto":"Sem a orientação e a proteção de seus professores, Harry, Rony e Hermione começam uma missão para destruir as Horcruxes, que são fontes da imortalidade de Voldemort. Mais do que nunca, eles dependem uns dos outros, mas forças obscuras ameaçam separá-los."
+,"data":"19 de novembro de 2010"
+,"diretor":"David Yates"
+,"premios":"Oscar: Indicado em Melhor Direção de Arte e Melhores Efeitos Especiais/ VES Awards: Melhor Personagem Animado em Filme - Dobby/ Jameson Empire Awards: Melhor Filme de Ficção Científica/ Fantasia/ National Movie Awards: Melhor Filme de Fantasia/ MTV Movie Awards: Melhor Vilão - Tom Felton/ Teen Choice Awards 2011: Melhor filme de Ficção Científica/Fantasia e em Melhor atriz de filme de Ficção Científica/Fantasia - Emma Watson"}
+,{"id":8
+,"titulo":"Harry Potter e as Relíquias da Morte: P 2"
+,"img1":"./css/reliquiasdamorte4.jpg"
+,"img2":"./css/reliquiasdamorte5.jpg"
+,"texto":"No desfecho épico, a batalha entre as forças do bem e do mal da magia alcançam o mundo dos trouxas. O risco nunca foi tão grande e ninguém está seguro. Harry Potter fará um sacrifício final conforme o confronto com Lord Voldermort se aproxima."
+,"data":"15 de julho de 2011"
+,"diretor":"David Yates"
+,"premios":"People's Choice Award: Filme Favorito/ MTV Movie Awards: Melhor elenco - Daniel Radcliffe, Rupert Grint, Emma Watson e Tom Felton/ MTV Movie Award: Melhor Herói - Daniel Radcliffe/ Prêmio BAFTA de Cinema: Melhores Efeitos Visuais/ People's Choice Award: Filme de Ação Favorito/ People's Choice Award: Adaptação de Livro Favorita/ Critics' Choice Award: Melhor Maquiagem/ Empire Award: Melhor Filme/ muitos outros."
+}];
